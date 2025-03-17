fbbf30565e574a2d94a4f55dc749791dc6fa6056
Joppe-VH <joppe.vanhoof@gmail.com>
2025-03-17 14:11:01 +0100

    4. add todo

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,22 +1,38 @@
+import { useAddTodoMutation, useGetCategoriesQuery } from "@/store/todoApi"
 import { Button } from "./ui/button"
 import { Input } from "./ui/input"
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./ui/select"
 
 const AddTodo = () => {
+  const [addTodo, { isLoading }] = useAddTodoMutation()
+  const { data: categories } = useGetCategoriesQuery()
+  const handleAddTodo = (fd: FormData) => {
+    const todo = {
+      text: fd.get("text") as string,
+      categorieId: fd.get("categorieId") as string,
+    }
+    addTodo(todo)
+  }
+
   return (
-    <div className="flex w-full space-x-2">
-      <Input className="w-full" type="text" placeholder="Add a new todo..." />
-      <Select>
+    <form action={handleAddTodo} className="flex w-full space-x-2">
+      <Input className="w-full" type="text" name="text" placeholder="Add a new todo..." />
+      <Select name="categorieId">
         <SelectTrigger>
           <SelectValue placeholder="Category" />
         </SelectTrigger>
         <SelectContent>
-          <SelectItem value="work">Work</SelectItem>
-          <SelectItem value="personal">Personal</SelectItem>
+          {categories?.map(categorie => (
+            <SelectItem key={categorie.id} value={categorie.id}>
+              {categorie.name}
+            </SelectItem>
+          ))}
         </SelectContent>
       </Select>
-      <Button type="submit">Add</Button>
-    </div>
+      <Button type="submit" disabled={isLoading}>
+        Add
+      </Button>
+    </form>
   )
 }
 export default AddTodo
--- src/store/todoApi.ts
+++ src/store/todoApi.ts
@@ -1,5 +1,5 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
-import { Todo } from "../types"
+import { Todo, Categorie } from "../types"
 
 export const todoApi = createApi({
   reducerPath: "todoApi",
@@ -7,7 +7,16 @@ export const todoApi = createApi({
   tagTypes: ["Todos"],
   endpoints: builder => ({
     getTodos: builder.query<Todo[], void>({ query: () => "/todos?_embed=categorie" }),
+    addTodo: builder.mutation<Todo, Partial<Todo>>({
+      query: todo => ({
+        url: "/todos",
+        method: "POST",
+        body: todo,
+      }),
+      invalidatesTags: ["Todos"],
+    }),
+    getCategories: builder.query<Categorie[], void>({ query: () => "/categories" }),
   }),
 })
 
-export const { useGetTodosQuery } = todoApi
+export const { useGetTodosQuery, useAddTodoMutation, useGetCategoriesQuery } = todoApi
